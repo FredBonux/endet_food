ef319a50d8a54bd0dcc9b0fdd2dda6c1cb07fa3a
Federico Bono <federicobono99@gmail.com>
2020-03-21 19:28:33 +0100

    Updates

---
+++ precache-manifest.c651027dc09524ec437aabc1a9e9df70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a17d90d901dc87379cbedc23b7d248",
+    "url": "/endet_food-web/index.html"
+  },
+  {
+    "revision": "90ec185d4b6e36273bd2",
+    "url": "/endet_food-web/static/css/main.757bce29.chunk.css"
+  },
+  {
+    "revision": "ae69e87dac809cc32d15",
+    "url": "/endet_food-web/static/js/2.bb0917b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/endet_food-web/static/js/2.bb0917b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ec185d4b6e36273bd2",
+    "url": "/endet_food-web/static/js/main.6fccb3f8.chunk.js"
+  },
+  {
+    "revision": "98bb41c7e9d0bd44193d",
+    "url": "/endet_food-web/static/js/runtime-main.6b75841e.js"
+  },
+  {
+    "revision": "14a87a511a48c58ca4ac322b7391ca5a",
+    "url": "/endet_food-web/static/media/basket.14a87a51.svg"
+  },
+  {
+    "revision": "4b5ac6ec05b285ce7715a64a377fb940",
+    "url": "/endet_food-web/static/media/form.4b5ac6ec.svg"
+  },
+  {
+    "revision": "b6c24470254f8dd70167d885e6abf1d6",
+    "url": "/endet_food-web/static/media/home.b6c24470.svg"
+  },
+  {
+    "revision": "bbfcf69415b95aee34d301e10736026b",
+    "url": "/endet_food-web/static/media/plus.bbfcf694.svg"
+  },
+  {
+    "revision": "ad672166a8b231bd1f047d38872a3d80",
+    "url": "/endet_food-web/static/media/settings.ad672166.svg"
+  }
+]);
